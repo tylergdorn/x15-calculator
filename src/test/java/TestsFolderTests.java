014c4e5cd30ee802093e913936774f08b37490a1
Tyler Dorn <tylergdorn@gmail.com>
2019-04-28 14:54:58 -1000

    working testing framework

--- src/test/java/TestsFolderTests.java
+++ src/test/java/TestsFolderTests.java
@@ -1,21 +1,34 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class test {
-    @Test
-    public void testsFolderTests(){
+@RunWith(Parameterized.class)
+public class TestsFolderTests {
+    private String expectedPath;
+    private String inputPath;
+
+    public TestsFolderTests(String inputPath, String expectedPath){
+        this.expectedPath = expectedPath;
+        this.inputPath = inputPath;
+    }
+
+    @Parameterized.Parameters(name = "{index}: fib({0})={1}")
+    public static Collection<String[]> testsFolderFinder(){
         File folder = new File("tests");
         System.out.println(folder.getAbsolutePath());
         File[] listOfTestFolders = folder.listFiles();
+        Collection<String[]> output = new ArrayList<>();
         // this matches a filename with the necessary file endings
         Pattern filename = Pattern.compile("test\\.(txt|out|err)");
         // make sure that the tests folder is there and there are folders in it
@@ -27,16 +40,16 @@ public class test {
                 File[] inputFiles = new File[2];
                 if (listOfFiles != null) {
                     // look through all the files inside each folder
-                    for (int i = 0; i < listOfFiles.length; i++) {
-                        Matcher matcher = filename.matcher(listOfFiles[i].getName());
+                    for (File file : listOfFiles) {
+                        Matcher matcher = filename.matcher(file.getName());
                         if (matcher.find()) {
                             // we want to know where the input is and where the output is.
                             if (matcher.group(1).equals("txt")) {
-                                inputFiles[0] = listOfFiles[i];
+                                inputFiles[0] = file;
                             }
                             // we don't really care if it's a .err or a .out because we compare them the same way.
                             else {
-                                inputFiles[1] = listOfFiles[i];
+                                inputFiles[1] = file;
                             }
                         } else {
                             System.out.println("unknown file in test folder: " + testFolder.getName());
@@ -44,15 +57,18 @@ public class test {
 
                     }
                 }
-                runTest(inputFiles[0].getAbsolutePath(), inputFiles[1].getAbsolutePath());
+//                runTest(inputFiles[0].getAbsolutePath(), inputFiles[1].getAbsolutePath());
+                output.add(new String[]{inputFiles[0].getAbsolutePath(), inputFiles[1].getAbsolutePath()});
             }
         }
+        return output;
     }
 
-    private void runTest(String inputFileName, String resultFileName){
+    @Test
+    public void runTest(){
         // load up our readers
-        TestReader input = new TestReader(inputFileName);
-        TestReader result = new TestReader(resultFileName);
+        TestReader input = new TestReader(inputPath);
+        TestReader result = new TestReader(expectedPath);
         // prepare our calculator parser
         Parser p = new Parser(input.read());
         List<String> output = new ArrayList<>();
@@ -62,7 +78,7 @@ public class test {
             output.add(Integer.toString(p.process()));
             // if it fails to run, capture and add the failed output to the list
         } catch (MalformedError e) {
-            output.add(String.valueOf(e.getCause()));
+            output.add(String.valueOf(e.getMessage()));
         }
 //       compare the expected output and the output we got
         for(int i = 0; i < output.size(); i++){
